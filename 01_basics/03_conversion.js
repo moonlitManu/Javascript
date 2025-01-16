6768cf795baaee1412ea0be132f0e43eae06628e
Manav Dev Sharma <manavdev202@gmail.com>
2025-01-16 05:55:36 +0000

    config container done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+let roll_no = "1"
+// console.log(typeof roll_no)
+// console.log(roll_no)
+
+let num_roll_no = Number(roll_no)
+
+// console.log(typeof num_roll_no)
+// console.log(num_roll_no)
+
+let isLoggedin = true
+let log = Number(isLoggedin)
+// console.log(typeof isLoggedin)
+// console.log(isLoggedin)
+
+// console.log(typeof Number(undefined))
+// console.log(typeof Number(null))
+
+let res = undefined
+// console.log(Number(undefined))
+
+// res = null
+// console.log(Number(res))
+
+// console.log(Number("21fa"))
+
+// console.log(Boolean(1))
+
+console.log(Boolean(""))
+console.log(Boolean("Manav"))
