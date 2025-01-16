585a9cb77972474c6d341f6e1644fc8f367664a4
Manav Dev Sharma <manavdev202@gmail.com>
2025-01-16 13:39:06 +0000

    config container done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+//Tricky comparision
+
+
+// Note: Such tricky comparision should be avoided
+
+// console.log("2"> 1)
+// console.log("2" == 2)
+
+// console.log(null == 0)// conversion is unpredictable
+// console.log(null > 0)
+// console.log(null >= 0) // here converted to 0
+// console.log(null <= 0) // here converted to 0
+
+console.log(undefined == 0)
+
+// Strict check === also check the type too 
+
+console.log(2 === 2)
+console.log("2" === 2)
